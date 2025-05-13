7e9875408deb0404b06f89c34d8ba86bbf5721b1
Juan Cortes <jcamilocoes@gmail.com>
2025-05-13 12:23:57 -0500

    JCC - minor fix in the Pool system and add the pool controller to the blocks controller

--- Assets/Scripts/Blocks/BlocksController.cs
+++ Assets/Scripts/Blocks/BlocksController.cs
@@ -1,14 +1,21 @@
+using JCC.Utils.Pool;
 using UnityEngine;
 
 namespace Scripts.Blocks 
 {
-    public class BlockController : MonoBehaviour
+    public class BlocksController : MonoBehaviour
     {
+        [SerializeField] private GameObject _initialBlock;
+
         private IMovement _blockMovement;
         private IFallMovement _fallMovement;
+        private IPoolController<BlockMonobehavior> _poolController;
 
         private void Start()
         {
+            _poolController = new PoolControllerImpl<BlockMonobehavior>();
+            _poolController.SetPoolObject(_initialBlock, 20, true);
+
             _blockMovement = GetComponent<IMovement>();
             _blockMovement.Initialization();
             _blockMovement.StartMovement();
@@ -17,17 +24,17 @@ namespace Scripts.Blocks
             _fallMovement.Initialization();
         }
 
-        public void StartFalling() 
-        {
-            _fallMovement.StartFalling();
-        }
-
         private void Update()
         {
             if (Input.GetMouseButtonDown(0))
             {
                 StartFalling();
             }
+        }
+
+        private void StartFalling() 
+        {
+            _fallMovement.StartFalling();
         }
     }
 }
